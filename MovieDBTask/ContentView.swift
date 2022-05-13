344a00fbd2ade4ca9216c3c8e55dc5131d5f3d47
Bharath <k.bharath@concentrix.com>
2022-05-13 13:15:07 +0530

    Initial Commit

---
+++ MovieDBTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieDBTask
+//
+//  Created by Barath K on 13/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
